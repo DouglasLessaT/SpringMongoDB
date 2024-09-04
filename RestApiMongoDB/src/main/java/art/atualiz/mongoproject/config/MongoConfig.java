9a5bcc60a121361d1b2c28e9029aa84c8b2693d1
MAGOCIZENTO <douglaslessat@gmail.com>
2024-09-04 19:27:05 -0300

    modificando mongo config

--- RestApiMongoDB/src/main/java/art/atualiz/mongoproject/config/MongoConfig.java
+++ RestApiMongoDB/src/main/java/art/atualiz/mongoproject/config/MongoConfig.java
@@ -1,35 +1,25 @@
 package art.atualiz.mongoproject.config;
 
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 
 /**
- * Configuração necessária para usar índices no MongoDB
- * Adicionada a propriedade condicional se está usando instância local ou no Atlas
- * Localmente, funciona o índice, mas no Atlas (FREE) não.
+ * configuranção necessária para usar indices no mongodb
+ * adicionada a propriedade condicional se esta usando instancia local ou no atlas
+ * localmente funciona o index mas no atlas (FREE) não
  */
 @ConditionalOnProperty(value = "app.use.mongolocal", havingValue = "true")
 @Configuration
 public class MongoConfig extends AbstractMongoClientConfiguration {
+ @Override
+ protected boolean autoIndexCreation() {
+  return true;
+ }
 
-    @Override
-    protected String getDatabaseName() {
-        return "MongoProject2";
-    }
+ @Override
+ protected String getDatabaseName() {
+  return "MongoProject2";
+ }
 
-    @Override
-    protected boolean autoIndexCreation() {
-        return true;
-    }
-
-    @Bean
-    @Override
-    public MongoClient mongoClient() {
-        // Configure o cliente MongoDB local
-        return MongoClients.create("mongodb://127.0.0.1:27017");
-    }
 }
